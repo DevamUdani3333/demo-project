6661354d5a7e3078598d40e6b52fff7bec081fea
Devam <devamudnai3333@gmail.com>
2025-08-13 15:21:14 +0530

    Added backend and Frontend with Docker setup

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  forecasts: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('/api/weatherforecast')
+      .subscribe(data => {
+        this.forecasts = data;
+      });
+  }
+}
